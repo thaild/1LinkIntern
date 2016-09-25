cf1ff688feabc9b4d7d7b82135d8d4a89e941d1d
thailee <thaiattt@gmail.com>
2016-09-25 18:13:34 +0700

    student ver 2
    student ver 2

---
+++ week1ver2/model/student.py
@@ -0,0 +1,20 @@
+class student(object):
+	id = ''
+	name = ''
+	add = ''
+	gender = ''
+	math = 0
+	physics = 0
+	chemistry = 0
+
+	def __init__(self, id, name, add, gender, math, chemistry, physics):
+		self.id = id
+		self.name = name
+		self.add = add
+		self.gender = gender
+		self.math = math
+		self.chemistry =  chemistry
+		self.physics = physics
+
+	def _get_input(self):
+		se
